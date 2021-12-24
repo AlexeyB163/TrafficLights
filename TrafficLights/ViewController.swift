3192f9c87ceb98f701b4ddb0917336ec8b61d762
AlexeyB163 <sourceaisa@mail.ru>
2021-12-24 17:13:12 +0400

    logic completed

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redColorView: UIView!
+    @IBOutlet weak var yellowColorView: UIView!
+    @IBOutlet weak var greenColorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.layer.cornerRadius = 40
     }
 
-
+    
+    @IBAction func startButtonAction(_ sender: UIButton) {
+        if redColorView.alpha < 1 && yellowColorView.alpha < 1 && greenColorView.alpha < 1 {
+            redColorView.alpha = 1
+            sender.setTitle("Next", for: .normal)
+            return
+        }
+        if redColorView.alpha == 1  {
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1
+            return
+        }
+        if yellowColorView.alpha == 1 {
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 1
+            return
+        }
+        if greenColorView.alpha == 1 {
+            greenColorView.alpha = 0.3
+            redColorView.alpha = 1
+            return
+        }
+        
+    }
+    
 }
 
