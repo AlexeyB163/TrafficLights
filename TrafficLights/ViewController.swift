988b94dbf19ccaef83834b70678c8e0984892543
AlexeyB163 <sourceaisa@mail.ru>
2021-12-25 01:14:11 +0400

    don't working changing font for button

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -12,22 +12,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var redColorView: UIView!
     @IBOutlet weak var yellowColorView: UIView!
     @IBOutlet weak var greenColorView: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.layer.cornerRadius = 10
+        
         redColorView.layer.cornerRadius = redColorView.layer.frame.width / 2
         yellowColorView.layer.cornerRadius = yellowColorView.layer.frame.width / 2
         greenColorView.layer.cornerRadius = greenColorView.layer.frame.width / 2
     }
 
-    
     @IBAction func startButtonAction(_ sender: UIButton) {
+        
+
+        
         if redColorView.alpha < 1 && yellowColorView.alpha < 1 && greenColorView.alpha < 1 {
-            redColorView.alpha = 1
             sender.setTitle("Next", for: .normal)
+            redColorView.alpha = 1
+
             return
         }
         if redColorView.alpha == 1  {
